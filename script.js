0f2c6c22bf40be2911e5c15d4f7c9a2a7083e585
Stephanie Raad <stephy.raad@gmail.com>
2014-04-23 14:20:27 -0700

    winning logic

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-/*function gameController($scope){
-	$scope.grid= new grid();
-	function grid($scope){
-		this.rows=[['0','0','0'],['0','0','0'],['0','0','0']];
-	}
+// flashScreen = function(reps, pause, delay, color) {
+// 	if (reps == 0)
+// 		return false;
+		
+// 	h1Title = $('h1').css('color');
+// 	$('h1').css('color', color);
+// 	setTimeout("$('h1').css('color', h1);", pause);
+// 	_reps = reps;
+// 	_pause = pause;
+// 	_delay = delay;
+// 	_color = color;
+		
+// 	setTimeout('flashScreen(_reps - 1, _pause, _delay, _color);', _delay);
+// }				
+// $(document).ready(function() { flashScreen(10, 100, 300, '#a00'); });
 
 
 
-	" {cellclick: c == 1, cellclick2: c== 2}">
-}*/
 
 function gameController($scope){
 	$scope.rows=[['','',''],['','',''],['','','']];	
 	var player= 0;
 	$scope.check=function(r,c){
-		if (player == 1) {
+		if (player === 1) {
 		// console.log($scope.rows[r][c]);
 			$scope.rows[r][c]= 'X';
 			console.log(r, c); // 1
@@ -23,5 +31,33 @@ function gameController($scope){
 			console.log(r, c);
 			player = player%2 + 1;
 		}
-	};
+
+		function matchrow(rowNum){
+			if (rows[rowNum][0]==rows[rowNum][1] && rows[rowNum][1]==rows[rowNum][2]){ 
+					alert("You Win");
+			}
+			}//end of function matchrow
+
+		function matchcol(colNum){
+		if (rows[0][colNum]==rows[1][colNum] && rows[1][colNum]==rows[2][colNum]){
+			
+			alert("You Win");
+		}
+	}//end of matchcol
+
+
+	function matchDiag(diagNum){
+		var x=false;
+		if(rows[0][0]== rows[1][1] && rows[1][1]==rows[2][2]){
+			alert("You Win") ;
+		}
+		else if (rows[2][0]==rows[1][1] && rows[1][1]==rows[0][2]){
+			alert("You Win");
+		}	
+		
+	}//end of matchdiag
+//////////////////////////////////
+};// end of function check
 }
+/////////////
+	
