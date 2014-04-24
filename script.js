2337e47d21a3eb215cd3f74b6d6d6a4b819d11e6
Stephanie Raad <stephy.raad@gmail.com>
2014-04-23 17:24:06 -0700

    2 of my winning conditions work

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// flashScreen = function(reps, pause, delay, color) {
-// 	if (reps == 0)
-// 		return false;
-		
-// 	h1Title = $('h1').css('color');
-// 	$('h1').css('color', color);
-// 	setTimeout("$('h1').css('color', h1);", pause);
-// 	_reps = reps;
-// 	_pause = pause;
-// 	_delay = delay;
-// 	_color = color;
-		
-// 	setTimeout('flashScreen(_reps - 1, _pause, _delay, _color);', _delay);
-// }				
-// $(document).ready(function() { flashScreen(10, 100, 300, '#a00'); });
-
-
 
 
 function gameController($scope){
@@ -24,40 +7,46 @@ function gameController($scope){
 		if (player === 1) {
 		// console.log($scope.rows[r][c]);
 			$scope.rows[r][c]= 'X';
-			console.log(r, c); // 1
+			
 			player = player%2 + 1;
 		} else { 
 			$scope.rows[r][c]= 'O';
 			console.log(r, c);
 			player = player%2 + 1;
 		}
+		matchrow(r);
+		matchcol(c);
+		matchDiag(r);
+		};//end of check function
 
 		function matchrow(rowNum){
-			if (rows[rowNum][0]==rows[rowNum][1] && rows[rowNum][1]==rows[rowNum][2]){ 
+			console.log($scope.rows[rowNum]);
+			if ($scope.rows[rowNum][0]==$scope.rows[rowNum][1] && $scope.rows[rowNum][1]==$scope.rows[rowNum][2]){ 
+					console.log("it works");
 					alert("You Win");
 			}
 			}//end of function matchrow
 
 		function matchcol(colNum){
-		if (rows[0][colNum]==rows[1][colNum] && rows[1][colNum]==rows[2][colNum]){
-			
+		if ($scope.rows[0][colNum]==$scope.rows[1][colNum] && $scope.rows[1][colNum]==$scope.rows[2][colNum]){
+			onsole.log("it works");
 			alert("You Win");
 		}
 	}//end of matchcol
 
 
-	function matchDiag(diagNum){
+	function matchDiag(){
 		var x=false;
-		if(rows[0][0]== rows[1][1] && rows[1][1]==rows[2][2]){
+		if($scope.rows[0][0]== $scope.rows[1][1] && $scope.rows[1][1]==$scope.rows[2][2]){
 			alert("You Win") ;
 		}
-		else if (rows[2][0]==rows[1][1] && rows[1][1]==rows[0][2]){
+		else if ($scope.rows[2][0]==$scope.rows[1][1] && $scope.rows[1][1]==$scope.rows[0][2]){
 			alert("You Win");
 		}	
 		
 	}//end of matchdiag
 //////////////////////////////////
-};// end of function check
+// end of function check
 }
 /////////////
 	
